e66cb01eaf036c185d4d5ec14b74360fbefd5c25
mikacapile <mikaely.capile@estudante.ifms.edu.br>
2026-02-25 10:20:10 -0400

    aula25

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,38 @@ let some:string = "Guilherme"
 
 let vetor:number[] = [1,2,3,4,5]
 
-let vetorString:string[] = ["oi","olá"]
+let nomes:string[] = ["oi","olá"]
+
 console.log(nomes)
+
+
+let bol: boolean = true;
+
+bol = false
+
+const variavel:any = "oi"
+
+const a:undefined = undefined;
+
+const obj:{id:number;nome:string;idade:number} = {
+    id:1,
+    nome:"Mika",
+    idade: 17
+}
+obj.id=2
+obj.nome = "Mika"
+obj.idade = 32
+
+const x1 = 10
+
+const vetor1 = [0,1,2,3,4]
+vetor1[0] = 10
+
+let vetor2: number[]= [0,1,2,3,4]
+vetor2 = [0,1,2,3,4]
+
+let outraletra: number|string[] = 10 ;
+
+
+
+
